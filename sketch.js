deb68cb186cf0dfeb742ec312a25a586a91f8a87
Sebastian-Barron <67523744+Sebastian-Barron@users.noreply.github.com>
2021-03-16 22:43:12 -0500

    fixed some spelling

--- sketch.js
+++ sketch.js
@@ -1,1 +1,1 @@
-alert("This game is meant to play as an interview/interogation, there are multiple answers that lead to multiple branches in the story. Enjoy!")
+alert("This game is meant to play as an interview/interrogation, there are multiple answers that lead to multiple branches in the story. Enjoy!")
